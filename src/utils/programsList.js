ca94bcb78e1efdf7f4943acc74dd366df9f67e37
Dhithen <calvaroj@uninorte.edu.co>
2021-07-24 16:12:24 -0500

    Imagenes añadidas, tarjetas editadas

--- src/utils/programsList.js
+++ src/utils/programsList.js
@@ -1,8 +1,12 @@
+import charla from "../assets/images/Programs/charlas.jpg"
+import orientacion from "../assets/images/Programs/orientacion.jpg"
+import retiro from "../assets/images/Programs/retiro.jpg"
+
 export const programsList = [{
         id: 1,
         programName: "Preparación para la jubilación",
         description: "Busca apoyar a los trabajadores que están próximos a su etapa de jubilación, brindando estrategias y consejos para que estos creen un nuevo proyecto de vida.",
-        img: "https://images.unsplash.com/photo-1567067974934-75a3e4534c14?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        img: retiro,
     },
     {
         id: 2,
@@ -20,31 +24,31 @@ export const programsList = [{
         id: 4,
         programName: "Coaching",
         description: "Es una técnica para el fortalecimiento de potenciales enfocados al logro de las metas individuales y grupales.",
-        img: "https://images.unsplash.com/photo-1529070538774-1843cb3265df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        img: "https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
     },
     {
         id: 5,
         programName: "Liderazgo",
         description: "Es una técnica para el fortalecimiento de potenciales enfocados al logro de las metas individuales y grupales.",
-        img: "https://images.unsplash.com/photo-1529070538774-1843cb3265df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        img: "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
     },
     {
         id: 6,
         programName: "Trabajo en equipo",
         description: "Es una técnica para el fortalecimiento de potenciales enfocados al logro de las metas individuales y grupales.",
-        img: "https://images.unsplash.com/photo-1529070538774-1843cb3265df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        img: "https://images.unsplash.com/photo-1552664730-d307ca884978?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
     },
     {
         id: 7,
         programName: "Manejo del estrés",
         description: "Es una técnica para el fortalecimiento de potenciales enfocados al logro de las metas individuales y grupales.",
-        img: "https://images.unsplash.com/photo-1529070538774-1843cb3265df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        img: "https://images.unsplash.com/photo-1621252179027-94459d278660?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
     },
     {
         id: 8,
         programName: "Atención y servicio al cliente",
         description: "Es una técnica para el fortalecimiento de potenciales enfocados al logro de las metas individuales y grupales.",
-        img: "https://images.unsplash.com/photo-1529070538774-1843cb3265df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        img: "https://images.unsplash.com/photo-1525182008055-f88b95ff7980?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
     },
 ];
 
@@ -52,7 +56,7 @@ export const psicoeducativos = [{
         id: 1,
         programName: "Charlas, seminarios y talleres",
         description: "Son actividades formativas e informativas de corta duración que tienen como objetivo educar en los siguientes temas: asertividad, manejo de estrés, manejo del duelo y reestructuración del pensamiento negativo.",
-        img: "https://images.pexels.com/photos/1311518/pexels-photo-1311518.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        img: charla,
     },
     {
         id: 2,
@@ -70,13 +74,13 @@ export const psicoeducativos = [{
         id: 4,
         programName: "Preparación a entrevistas",
         description: "Es un programa cuyo objetivo es llevar a las personas a tener un conocimiento más profundo de sí mismos y que en entrevistas laborales proyecten mayor seguridad.",
-        img: "https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        img: "https://images.pexels.com/photos/1311518/pexels-photo-1311518.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     },
     {
         id: 5,
         programName: "Cineforos",
         description: "Es una actividad que se realiza con el objetivo de, a través de películas o documentales, realizar análisis de la salud emocional.",
-        img: "https://images.pexels.com/photos/5940704/pexels-photo-5940704.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        img: "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80",
     },
     {
         id: 6,
@@ -88,7 +92,7 @@ export const psicoeducativos = [{
         id: 7,
         programName: "Programa de orientación ocupacional",
         description: "Es un programa grupal con la finalidad de facilitar la toma de dicisiones académicas y profesionales.",
-        img: "https://images.pexels.com/photos/1311518/pexels-photo-1311518.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        img: orientacion,
     },
 
 ];
