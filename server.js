1c2a3a62f6b57960b32f54eede29c5971a687e0c
Dhithen <calvaroj@uninorte.edu.co>
2021-07-27 15:43:28 -0500

    actualización

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+var path = require('path');
+var express = require('express');
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html')),
-        function(err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        };
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('listening on port ', server.address().port);
 });
