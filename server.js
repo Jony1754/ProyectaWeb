b6f02a96a5dd63cf808086f98341542bd4cec352
Dhithen <calvaroj@uninorte.edu.co>
2021-10-08 20:59:12 -0500

    https protocol

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.get('*', function(req, res, next) {
     if (req.headers['x-forwarded-proto'] != 'https')
-        res.redirect('https://https://proyectapotencialeshumanos.com' + req.url)
+        res.redirect('https://proyectapotencialeshumanos.com' + req.url)
     else
         next() /* Continue to other routes if we're not redirecting */
 })
