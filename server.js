14c8e274c7a49c10eb93730afa884102cea7ca89
Dhithen <calvaroj@uninorte.edu.co>
2021-10-08 20:43:27 -0500

    https protocol

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
+app.use(function(req, res, next) {
+    if ((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect(307, 'https://' + req.get('Host') + req.url);
+    } else
+        next();
+});
+
 var server = app.listen(app.get('port'), function() {
     console.log('listening on port ', server.address().port);
 });
