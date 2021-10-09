a413cae1dea8423596074d214e134a148444d6ea
Dhithen <calvaroj@uninorte.edu.co>
2021-10-08 21:05:59 -0500

    https protocl failed to solve

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ var express = require('express');
 
 var app = express();
 
-app.get('*', function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] != 'https')
-        res.redirect('https://proyectapotencialeshumanos.com' + req.url)
-    else
-        next() /* Continue to other routes if we're not redirecting */
-})
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
